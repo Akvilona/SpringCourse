d589d411924f0a12abc2883313af51cec8195c9e
Andrey Antonov <akvilona@list.ru>
2022-08-15 11:25:19 +0300

    Lessont 84 Ok

--- FirstSecurityApp/src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.alishev.springcourse.FirstSecurityApp.services.PersonDetailService;
@@ -41,13 +42,16 @@ public class  SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /** метод настраивает аутентификацию */
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailService);
+
+        /** для того чтобы использовать шифрование добавляем .passwordEncoder() */
+        auth.userDetailsService(personDetailService).passwordEncoder(getPasswordEncoder());
     }
 
     /** указываем что пароль ни как не шифруется */
     @Bean
     public PasswordEncoder getPasswordEncoder () {
-        return NoOpPasswordEncoder.getInstance();
+
+        return new BCryptPasswordEncoder();
     }
 
 }
--- FirstSecurityApp/src/main/java/ru/alishev/springcourse/FirstSecurityApp/services/RegistrationService.java
+++ FirstSecurityApp/src/main/java/ru/alishev/springcourse/FirstSecurityApp/services/RegistrationService.java
@@ -4,6 +4,7 @@
 package ru.alishev.springcourse.FirstSecurityApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.alishev.springcourse.FirstSecurityApp.models.Person;
@@ -12,14 +13,21 @@ import ru.alishev.springcourse.FirstSecurityApp.repositories.PeopleRepository;
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        /** получаем пароль с формы и получаем его
+         * шифрованный код и обновляем пароль у пользователя */
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+
+        /** сохраняем пользователя и пароль */
         peopleRepository.save(person);
     }
 }
